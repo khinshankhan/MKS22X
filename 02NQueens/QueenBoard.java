4fc68aba42f3d1b8b92efaff32c81947934fab50
Khinshan Khan <kkhan@stuy.edu>
2017-02-09 15:05:16 -0500

    start of file and cp of basic structure

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,41 @@
+public class QueenBoard{
+    private int[][]board;
+    private int solutionCount;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+    }
+
+    /**
+     *precondition: board is filled with 0's only.
+     *@return false when the board is not solveable. true otherwise.
+     *postcondition: 
+     *if false: board is still filled with 0's
+     *if true: board is filled with the 
+     *final configuration of the board after adding 
+     *all n queens. Uses solveH
+     */
+    public boolean solve()
+    {
+	return solveH(0);
+    }
+
+    private boolean solveH(int col){
+	return false;
+    }
+
+    /**
+     *@return the number of solutions found, or -1 if the board was never solved.
+     *The board should be reset after this is run.    
+     */
+    public int getSolutionCount(){
+    	return -1;
+    }
+    /**toString
+     *and all nunbers that represent queens are replaced with 'Q' 
+     *all others are displayed as underscores '_'
+     */
+    public String toString(){
+    	return "";
+    }
+}
