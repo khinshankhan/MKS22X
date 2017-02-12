44aa805fcbabf1f1e0c4f938e386853f11dd6c18
Khinshan Khan <kkhan@stuy.edu>
2017-02-11 22:45:14 -0500

    DONE WITH 1 SOLUTION, i'll do count later

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,7 +1,38 @@
 public class Driver{
     public static void main(String[]args){
-	QueenBoard a= new QueenBoard(2);
+	QueenBoard a= new QueenBoard(0);
 	System.out.println(a.getSolutionCount());
+	a.solve();
 	System.out.println(a.toString());
+	
+	QueenBoard b= new QueenBoard(1);
+	System.out.println(b.getSolutionCount());
+	b.solve();
+	System.out.println(b.toString());
+
+	QueenBoard c= new QueenBoard(2);
+	System.out.println(c.getSolutionCount());
+	c.solve();
+	System.out.println(c.toString());
+
+	QueenBoard d= new QueenBoard(3);
+	System.out.println(d.getSolutionCount());
+	d.solve();
+	System.out.println(d.toString());
+
+	QueenBoard e= new QueenBoard(4);
+	System.out.println(e.getSolutionCount());
+	e.solve();
+	System.out.println(e.toString());
+
+	QueenBoard f= new QueenBoard(5);
+	System.out.println(f.getSolutionCount());
+	f.solve();
+	System.out.println(f.toString());
+
+	QueenBoard g= new QueenBoard(6);
+	System.out.println(g.getSolutionCount());
+	g.solve();
+	System.out.println(g.toString());
     }
 }
--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -24,11 +24,44 @@ public class QueenBoard{
      *all n queens. Uses solveH
      */
     public boolean solve(){
+	if(size<4)
+	    return false;
 	return solveH(0);
     }
     
     private boolean solveH(int col){
+	for(int r=0; r<size; r++){
+	    //base case
+	    if (col >= size)
+		return true;
+	    if(valid(r,col)){
+		setQueen(r,col);
+		if(solveH(col + 1)){
+		    return true;
+		}
+		else{
+		    remQueen(r,col);
+		}
+	    }
+	}
 	return false;
+    }
+
+    private void setQueen(int r, int c){
+	invalidator(r,c);
+    }
+
+    private void remQueen(int r, int c){
+	if(queen(r,c))
+	    purify(r,c);
+    }
+    
+    private boolean valid(int r, int c){
+	return board[r][c]==0;
+    }
+
+    private boolean queen(int r, int c){
+	return board[r][c]==-1;
     }
 
     private void invalidator(int r, int c){
@@ -54,8 +87,33 @@ public class QueenBoard{
 	for(int ro=r, co=c; ro<board.length&&co>=0; ro++, co--)
 	    board[ro][co]+=1;
 	//re places queen in position
-	board[r][c]=-1;
-	
+	board[r][c]=-1;	
+    }
+
+    private void purify(int r, int c){
+	//horizontal right
+	for(int co=c; co<board.length; co++)
+	    board [r][co]-=1;
+	//vertical down
+	for(int ro=r; ro<board.length; ro++)
+	    board[ro][c]-=1;
+	//horizontal left
+	for(int co=c; co>=0; co--)
+	    board[r][co]-=1;
+	//vertical up
+	for(int ro=r; ro>=0; ro--)
+	    board[ro][c]-=1;
+	//VIO DIAGS
+        for(int ro=r, co=c; ro>=0&&co>=0; ro--, co--)
+	    board[ro][co]-=1;
+	for(int ro=r, co=c; ro<board.length&&co<board.length; ro++, co++)
+	    board[ro][co]-=1;
+	for(int ro=r, co=c; ro>=0&&co<board.length; ro--, co++)
+	    board[ro][co]-=1;
+	for(int ro=r, co=c; ro<board.length&&co>=0; ro++, co--)
+	    board[ro][co]-=1;
+	//re places queen in position
+	board[r][c]=0;	
     }
 
     /**
