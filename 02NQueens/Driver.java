304c58bcd1d1d3eccf9b83fb6f2cf58607f381d1
Khinshan Khan <kkhan@stuy.edu>
2017-02-13 20:38:28 -0500

    Better testing

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,38 +1,9 @@
 public class Driver{
     public static void main(String[]args){
-	QueenBoard a= new QueenBoard(-5);
+	QueenBoard a= new QueenBoard(Integer.parseInt(args[0]));
+	a.countSolutions();
+	System.out.println("num solutions: "+a.getSolutionCount());
 	a.solve();
-	System.out.println(a.getSolutionCount());
-	System.out.println(a.toString());
-	
-	QueenBoard b= new QueenBoard(0);
-	b.solve();
-	System.out.println(b.getSolutionCount());
-	System.out.println(b.toString());
-
-	QueenBoard c= new QueenBoard(1);
-	System.out.println(c.getSolutionCount());
-	c.solve();
-	System.out.println(c.toString());
-
-	QueenBoard d= new QueenBoard(3);
-	d.solve();
-	System.out.println(d.getSolutionCount());
-	System.out.println(d.toString());
-
-	QueenBoard e= new QueenBoard(4);
-	e.solve();
-	System.out.println(e.getSolutionCount());
-	System.out.println(e.toString());
-
-	QueenBoard f= new QueenBoard(5);
-	f.solve();
-	System.out.println(f.getSolutionCount());
-	System.out.println(f.toString());
-
-	QueenBoard g= new QueenBoard(10);
-	g.solve();
-	System.out.println(g.getSolutionCount());
-	System.out.println(g.toString());
+	System.out.println("Board:\n"+a.toString());
     }
 }
