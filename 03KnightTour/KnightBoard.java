2dc5e0296739ac520158fa8e6aba2efbc761752f
Khinshan Khan <kkhan@stuy.edu>
2017-02-17 14:50:09 -0500

    im lost

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -24,29 +24,31 @@ public class KnightBoard{
     }
 
     public void solve(){
-	solveH(0,0,1);
+        solveH(0,0,1);
 
     } 
 
     // level is the # of the knight
-    private boolean solveH(int row ,int col, int level){
-	if (level > sr * sc){
-	    solve=true;
-	    return true;}
-	if (row < 0 || row > sr - 1 || col < 0 || col > sc - 1){
-	    return false;}
+    private void solveH(int row ,int col, int level){
+	if (level > sr * sc)
+	    return ;
+	if (row < 0 || row > sr - 1 || col < 0 || col > sc - 1)
+	    return false;
 	if(board[col][row] == 0){
-           board[col][row] = level;
-	   if(takeAnL(row,col,level)){
-	       solve=false;
-	       return false;}
+	    level+=1;
+	    board[col][row] = level;
+	    if(takeAnL(row,col,level)){
+		return true;
+	    }
+	    else{
+		level-=1;
+		board[row][col]=0;
+	    }
 	}
-	board[row][col] = 0;
-	return solve;
+	return false;
     }
 
     private boolean takeAnL(int r, int c, int L){
-	L+=1;
 	return (solveH(r+2, c+1, L) || 
 		solveH(r+1, c+2, L) || 
 		solveH(r-2, c-1, L) ||
