1dd950516b645a8a0a4450f11960464cb1d518cd
Khinshan Khan <kkhan@stuy.edu>
2017-03-09 21:01:12 -0500

    test cases ready, will write silver code tmr

--- 05CowProblems/Driver.sh
+++ 05CowProblems/Driver.sh
@@ -1,7 +1,16 @@
 for x in $(seq 1 10)
 do
-	echo $x
-	#timeout 10 "java ${KnightBoard $x $x}"
-	java BronzeDriver makelake.$x.in
-	cat makelake.$x.out
+    echo "Bronze" $x
+    echo ""
+    java BronzeDriver makelake.$x.in
+    cat makelake.$x.out
+done
+
+for x in $(seq 1 10)
+do
+    echo "Silver" $x
+    echo ""
+    java SilverDriver ctravel.$x.in
+    cat ctravel.$x.out
+    echo ""
 done
