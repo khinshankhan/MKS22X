d39d1d58a73b428dbd15c24c394be954d9a60c13
Khinshan Khan <kkhan@stuy.edu>
2017-03-09 20:52:38 -0500

    silver and bronze test case pre set ups

--- 05CowProblems/BronzeDriver.java
+++ 05CowProblems/BronzeDriver.java
@@ -1,9 +1,8 @@
-public class SilverDriver{
+public class BronzeDriver{
     public static void main(String args[]){
 	USACO x=new USACO();
 	if(args.length>0){
-	    System.out.println(x.silver(args[0]));
+	    System.out.println(x.bronze(args[0]));
 	}else{System.out.println("Invalid file");}
-    }
-	    
+    }	    
 }
--- 05CowProblems/Driver.sh
+++ 05CowProblems/Driver.sh
@@ -2,6 +2,6 @@ for x in $(seq 1 10)
 do
 	echo $x
 	#timeout 10 "java ${KnightBoard $x $x}"
-	java SilverDriver makelake.$x.in
+	java BronzeDriver makelake.$x.in
 	cat makelake.$x.out
 done
