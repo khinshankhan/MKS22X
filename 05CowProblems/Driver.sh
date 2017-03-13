7316fc0163a8154206f130467394cdc0c7a26520
Khinshan Khan <kkhan@stuy.edu>
2017-03-13 01:34:52 -0400

    Driver modified to accomodate finished silver

--- 05CowProblems/Driver.sh
+++ 05CowProblems/Driver.sh
@@ -1,11 +1,19 @@
+echo "*"
+echo "********"
+echo "*BRONZE*"
+echo "********"
+echo ""
 for x in $(seq 1 10)
 do
-    echo "Bronze" $x
-    echo ""
     java BronzeDriver makelake.$x.in
     cat makelake.$x.out
 done
 
+echo "*"
+echo "********"
+echo "*SILVER*"
+echo "********"
+echo ""
 for x in $(seq 1 10)
 do
     echo "Silver" $x
--- 05CowProblems/USACO.java
+++ 05CowProblems/USACO.java
@@ -8,7 +8,7 @@ public class USACO {
     }
 
     private int[][] lake;
-    private char[][] pasture;
+    private int[][][] pasture;
     //int m=0;
     //String debug="1";
     private ArrayList<int[]> instructions;
@@ -157,6 +157,7 @@ public class USACO {
     public int silver(String filename) {
 	int x=0;
 	try {
+	    int debug=0;
 	    File f = new File( filename );
 	    Scanner in = new Scanner( f );
 	    String fL = in.nextLine();
@@ -165,20 +166,72 @@ public class USACO {
 	    int rows = Integer.parseInt( firstLine[0] );
 	    int cols = Integer.parseInt( firstLine[1] );
 	    int t= Integer.parseInt(firstLine[2]);
-	    //debug+="s";//=1;
-	    pasture = new char[ rows ][ cols ];
+	    //x+=1;//=1;
+	    pasture = new int [ t+1 ] [ rows ][ cols ];
+	    //x+=1;
+	    //System.out.println(pasture[0].length+","+pasture[0][0].length);
+	    //System.out.println(printer2(0));
 	    for (int i = 0; i < rows; i++) {
-		String[] numbers = in.nextLine().split(" ");
+		//String[] numbers = in.nextLine().split(" ");
+		char[] numbers = in.nextLine().toCharArray();
+		//System.out.println(numbers.length+":"+cols);
 		for (int j= 0;j < cols; j++) {
-		    pasture[ i ][ j ] =numbers[j].charAt(0);
-		    //debug+="s";//+=9;
+		    //System.out.println(i+","+j);
+		    //System.out.println(printer2(0));
+		    if(numbers[j]=='*')
+			pasture[0][i][j]=-1;
 		}
 	    }
-	    System.out.println(printer2());
+	    //x+=1;
+	    String[] nums = in.nextLine().split(" ");
+	    int sr = Integer.parseInt( nums[0] );
+	    int sc = Integer.parseInt( nums[1] );
+	    int er= Integer.parseInt ( nums[2] );
+	    int ec= Integer.parseInt ( nums[3] );
+	    pasture[0][sr-1][sc-1]=1;
+	    //System.out.println(pasture.length);
+	    silverH();
+	    //System.out.println(printer2(6));
+	    //System.out.println(pasture[t][er-1][ec-1]);
+	    x=pasture[t][er-1][ec-1];
 	}catch(Exception e){
 	    System.out.println(" Invalid file");
+	    //e.printStackTrace();
 	    System.exit(0);}
 	return x;
+    }
+    public void silverH(){
+	for(int t=0; t< pasture.length-1; t++){
+	    //System.out.println(1);
+	    for (int i = 0; i < pasture[0].length; i++) {
+		//System.out.println(2);
+		for (int j= 0;j < pasture[0][0].length; j++) {
+		    //System.out.println(3);
+		    if(pasture[t][i][j]==-1){
+			pasture[t+1][i][j]=-1;
+		        continue;
+		    }
+		    if(pasture[t][i][j]!=0)
+		        continue;
+		    pasture[t+1][i][j]=askNeighbor(t,i,j);
+		}
+	    }
+	    //System.out.println(printer2(t));
+	}
+    }
+
+    public int askNeighbor(int t, int r,int c){
+	int sum=0;
+
+	if(r-1>-1 && pasture[0][r-1][c]!=-1)
+	    sum+=pasture[t][r-1][c];
+	if(r+1<pasture[0].length && pasture[0][r+1][c]!=-1)
+	    sum+=pasture[t][r+1][c];
+	if(c-1>-1 && pasture[0][r][c-1]!=-1)
+	    sum+=pasture[t][r][c-1];
+	if(c+1<pasture[0][0].length && pasture[0][r][c+1]!=-1)
+	    sum+=pasture[t][r][c+1];
+	return sum;
     }
     
     public String printer(){
@@ -190,15 +243,13 @@ public class USACO {
 	}
 	return s;
     }
-
-    public String printer2(){
+    public String printer2(int x){
 	String s="";
-	for(int i=0; i<pasture.length;i++){
-	    for(int j=0; j< pasture[0].length;j++)
-		s+=pasture[i][j]+" ";
+	for(int i=0; i<pasture[x].length;i++){
+	    for(int j=0; j< pasture[x][0].length;j++)
+		s+=pasture[x][i][j]+" ";
 	    s+="\n";
 	}
 	return s;
-    }
-		    
+    }  
 }
