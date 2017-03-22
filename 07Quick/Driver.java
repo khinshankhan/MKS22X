c40e85f609ef51ed1f7a88ffb6f82f9684346646
Khinshan Khan <kkhan@stuy.edu>
2017-03-22 00:49:01 -0400

    qsort done, need to fix oveflow in qselect

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -12,6 +12,7 @@ public class Driver{
     }
     
     public static void main(String [] args){
+	
 	//Partition Test Cases (Need to check the array visually)
 	/*int []data= randomArray(10, 100);
 	  System.out.println(Arrays.toString(data));
@@ -21,34 +22,50 @@ public class Driver{
 	//K's Test Cases
 	/*
 	  int[]ary = { 2, 10, 15, 23, 0,  5};
-	  System.out.println(Quick.quickselect( ary , 0 )); //would return 0
-	  System.out.println(Quick.quickselect( ary , 1 )); //would return 2
-	  System.out.println(Quick.quickselect( ary , 2 )); //would return 5
-	  System.out.println(Quick.quickselect( ary , 3 )); //would return 10
-	  System.out.println(Quick.quickselect( ary , 4 )); //would return 15
-	  System.out.println(Quick.quickselect( ary , 5 )); //would return 23
+	  System.out.println(Quick.quickSelect( ary , 0 )); //would return 0
+	  System.out.println(Quick.quickSelect( ary , 1 )); //would return 2
+	  System.out.println(Quick.quickSelect( ary , 2 )); //would return 5
+	  System.out.println(Quick.quickSelect( ary , 3 )); //would return 10
+	  System.out.println(Quick.quickSelect( ary , 4 )); //would return 15
+	  System.out.println(Quick.quickSelect( ary , 5 )); //would return 23
 	*/
 	//Quick Select Test Cases, can just look to see if a line isn't spaced and is "ERROR"
 	//Might modify to give more insight, but what works works.
+	
 	for(int i=0; i<10; i++){
 	    //checking for a friend; 10 min and 20 max
 	    int size=10+(int)Math.floor(Math.random()*(20-10+1));
 	    int []data= randomArray(size, 1000);
 	    //data[7]=data[3]; //double check
-	    System.out.println("\n Original:"+Arrays.toString(data));
+	    //System.out.println("\n Original:"+Arrays.toString(data));
 	    int[]sorted=data.clone();
 	    //checking for a friend
 	    int randompos=(int)Math.floor(Math.random()*(data.length));
 	    Arrays.sort(sorted);
-	    System.out.println(" Sorted:"+Arrays.toString(sorted)+"\n For index: "+randompos+"\n");
-	    int q= Quick.quickselect(data, randompos);
+	    //System.out.println(" Sorted:"+Arrays.toString(sorted)+"\n For index: "+randompos+"\n");
+	    int q= Quick.quickSelect(data, randompos);
 	    if(q==data[randompos]){
-		System.out.println(String.format(" Woo! \n sortedSelect: %s : quickSelect: %s",
-						 sorted[randompos], q));
+		System.out.println("WOO!\n");//String.format(" Woo! \n sortedSelect: %s : quickSelect: %s",
+				   //sorted[randompos], q));
 	    }else{System.out.println(String.format("ERROR! \n sortedSelect: %s : quickSelect: %s",
 						   sorted[randompos], q));}
 	}
+	
 
+
+
+	//QuickSort Test Cases
+	/*
+	for(int i=0; i<10; i++){
+	    //run out of heap?
+	    //int []data= randomArray(249710013, 100);
+	    int []data= randomArray(9999999, 100);
+	    int[]realSort=data.clone();
+	    Quick.quickSort(data);
+	    Arrays.sort(realSort);
+	    System.out.println(Arrays.equals(data, realSort)+"\n");
+	}
+	*/
     }
 }
 
--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -9,15 +9,28 @@ public class Quick{
 	data[b] = store;
     }
     
-    public static int quickselect(int []data, int k){
-	//return the value that is the kth smallest value of the array. 
-	//use your partition method to help you accomplish this.
-	int pos=0;
+    //easy randomizer
+    public static int randInt(int min, int max) {
+
+	// Usually this can be a field rather than a method variable
+	Random rand = new Random();
+
+	// nextInt is normally exclusive of the top value,
+	// so add 1 to make it inclusive
+	int randomNum = rand.nextInt(max - min) + 1 + min;
+
+	return randomNum;
+    }
+
+    //return the value that is the kth smallest value of the array. 
+    //use your partition method to help you accomplish this.
+    public static int quickSelect(int[] data, int k){
+	/*
+        int pos=0;
 	int start=0;
 	int end=data.length-1;
 	
 	while(true){
-	    //System.out.println(Arrays.toString(data));
 	    int x = part(data, start, end);
 	    pos=x;
 	    if(k==pos)
@@ -28,62 +41,126 @@ public class Quick{
 		start = pos;
 	    }
 	}
-	//System.out.println(Arrays.toString(data));
 	return data[pos];
+	*/
+	//return qselect(data, k, 0, data.length-1);
+	return dutchPartition(data, k, 0, data.length-1);
     }
     
-    public static int[] part ( int [] data, int start, int end){
-	
-	/*
-	Random random = new Random();
-	System.out.println(start+":"+end+"\n"+Arrays.toString(data));
-	int pos = random.nextInt(end - start) + start;
-	
-	//testing for a friend
-	int pos=start+(int)Math.floor(Math.random()*(end-start+1));
-	//System.out.println(pos+";"+data[pos]);
-	//System.out.println(pos+":"+data[pos]);
-	//System.out.println(Arrays.toString(data)+"\n");
-	swap(data, pos, end); // move pivot to end
-	//System.out.println(Arrays.toString(data));
-	int currenti = start;
-	for(int i = start; i < end; i++) {
-	    //System.out.println(Arrays.toString(data));
-	    if(data[i] < data[end]) {
-		swap(data, currenti, i);
-		currenti++;
-	    }
+    public static int qselect ( int [] data, int k, int start, int end){
+	int pos = part(data, start, end);
+	if(k == pos){
+	    return data[pos];
+	}else if(k < pos){
+	    return qselect(data, k, 0, pos);
+	}else{
+	    return qselect(data, k, pos, data.length-1);
 	}
-	//System.out.println(Arrays.toString(data));
-	swap(data, end, currenti); // Move pivot to its final place
-	//System.out.println(Arrays.toString(data));
-	return currenti;
     }
-	*/
+    
+    public static int part(int[] data, int start, int end){
+    	int pos =randInt(start,end);
+    	int v = data[pos];
+    	swap(data, pos, end);
+    	int i = start;
+    	int j = end-1;
+    	while(i <= j){
+    	    if(data[i] < v){
+    		i++;
+    	    }else{
+    		swap(data, i, j);
+    		j--;
+    	    }
+    	}
+    	swap(data, i, end);
+    	return i;
 
-	//Dutch Flag Problem
+    }
 
-	int lt=start;
-	int i=start;
-	int gt=end;
-	int pos=start+(int)Math.floor(Math.random()*(end-start+1));
-	swap(data,0,pos);
-	int v=data
-	while(i<=gt)
-	    if(a[i]==v){
+    public static int dutchPartition(int[] data, int k, int start, int end){
+	//System.out.println(k+":"+start+";"+end);
+	int i = start;
+	int lt = start;
+	int gt = end;
+	int pos = randInt(start,end);
+	int v = data[pos];
+	swap(data, pos, end);
+	while(i <= gt){
+	    if(data[i] == v){
 		i++;
-	    }else if(a[i]<v){
+	    }else if(data[i] < v){
 		swap(data, i, lt);
 		i++;
 		lt++;
 	    }else{
 		swap(data, i, gt);
-		g--;
+		gt--;
 	    }
-	swap(data, 0, lt);
-	int[] posHolder=new int [2];
-	posHolder[0]=lt;
-	posHolder[1]=gt;
-	return posHolder;
+	}
+	if(k==lt || k==gt || (k>lt && k<gt))
+	    return data[k];
+        if (k < lt){
+            return dutchPartition(data, k, start, lt);
+        } else{
+            return dutchPartition(data, k, gt, end);
+        }
+    }
+    public static void quickSort(int[] data){
+	qsortH(data, 0, data.length-1);
+    }
+    
+    public static void qsortH ( int [] data, int start, int end){
+	//Used to be part 
+	/*
+	  Random random = new Random();
+	  System.out.println(start+":"+end+"\n"+Arrays.toString(data));
+	  int pos = random.nextInt(end - start) + start;
+	
+	  //testing for a friend
+	  int pos=start+(int)Math.floor(Math.random()*(end-start+1));
+	  //System.out.println(pos+";"+data[pos]);
+	  //System.out.println(pos+":"+data[pos]);
+	  //System.out.println(Arrays.toString(data)+"\n");
+	  swap(data, pos, end); // move pivot to end
+	  //System.out.println(Arrays.toString(data));
+	  int currenti = start;
+	  for(int i = start; i < end; i++) {
+	  //System.out.println(Arrays.toString(data));
+	  if(data[i] < data[end]) {
+	  swap(data, currenti, i);
+	  currenti++;
+	  }
+	  }
+	  //System.out.println(Arrays.toString(data));
+	  swap(data, end, currenti); // Move pivot to its final place
+	  //System.out.println(Arrays.toString(data));
+	  return currenti;
+	  }
+	*/
+
+	//Dutch Flag Problem
+        if(start == end)
+	    return;
+	int i = start;
+	int lt = start;
+	int gt = end;
+	int pos = randInt(start,end);
+	int v = data[pos];
+	swap(data, pos, end);
+	while(i <= gt){
+	    if(data[i] == v){
+		i++;
+	    }else if(data[i] < v){
+		swap(data, i, lt);
+		i++;
+		lt++;
+	    }else{
+		swap(data, i, gt);
+		gt--;
+	    }
+	}
+	if(lt != 0)
+	    qsortH(data, start, lt-1);
+	qsortH(data, gt, end);
     }
 }
