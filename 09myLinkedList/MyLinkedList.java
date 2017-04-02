02e9810e4e5654f1991bf3e093ec73dd751e11a5
Khinshan Khan <kkhan@stuy.edu>
2017-04-02 14:02:33 -0400

    phase i+ii done. need to add in exceptions

--- 09myLinkedList/MyLinkedList.java
+++ 09myLinkedList/MyLinkedList.java
@@ -23,9 +23,27 @@ public class MyLinkedList{
 	}
     }
     
-    public boolean add(int value){
+    public boolean addFirst(int value){
 	LNode a= new LNode(value,start);
 	start=a;
+	//size++;
+	return true;
+    }
+    
+    public boolean add(int value){
+	LNode current=start;
+	if(current==null){
+	    addFirst(value);
+	    //NodePrinter(start);
+	}else{
+	    LNode added= new LNode(value);
+	    //NodePrinter(added);
+	    while(current.next!=null){
+		//NodePrinter(current);
+		current= current.next;
+	    }
+	    current.next=added;
+	}
 	size++;
 	return true;
     }
@@ -34,16 +52,82 @@ public class MyLinkedList{
 	return size;
     }
 
+    public int get(int index){
+	LNode current=start;
+	while(index!=0){
+	    current= current.next;
+	    index--;
+	}
+	return current.val;
+    }
+
+    public int set(int index,int newValue){
+	LNode current=start;
+	int temp;
+	while(index!=0){
+	    current= current.next;
+	    index--;
+	}
+	temp=current.val;
+	current.val=newValue;
+	return temp;
+    }
+
+    public int indexOf(int value){
+	LNode current=start;
+	int index= 0;
+	while(current.next!=null){
+	    if(current.val==value){
+		return index;
+	    }
+	    current= current.next;
+	    index++;
+	}
+	if(current.val==value){
+	    return index;
+	}
+	return -1;
+    }
+
+    public int remove(int index){
+	LNode current=start;
+	int temp;
+	if(index==0){
+	    temp=start.val;
+	    start=start.next;
+	}else{
+	while(index!=1){
+	    current= current.next;
+	    index--;
+	}
+	temp=current.next.val;
+	if(current.next.next==null){
+	    current.next=null;
+	    //System.out.println(1);
+	}else{
+	    current.next=current.next.next;
+	    //System.out.println(2);
+	}
+	}
+	size--;
+	//System.out.println(this.toString());
+	return temp;
+    }
+    
     public String toString(){
 	String ans="[";
 	LNode current = start;
         while(current.next!=null){
-	//for(int i=0; i<size; i++){
+	    //for(int i=0; i<size; i++){
 	    ans+=" "+current.val+",";
 	    current = current.next;
 	}
 	ans+=" "+current.val+" ]";
 	return ans;
+    }
+ 
+    private void NodePrinter(LNode a){
+        System.out.println(String.format("LNode \n Val: %s\n Next: %s", a.val, a.next));
     }
 }
 
