b16eb4f94e42dd488545421140a386e27f27b106
Khinshan Khan <kkhan@stuy.edu>
2017-04-24 18:31:27 -0400

    Had fun fixing deprecation and unchecked or unsafe operations

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -1,77 +1,48 @@
+import java.util.Stack;
+
 public class StackCalc{
 
-    private static String apply(String op, String a, String b){
-
-	double inputA = Double.parseDouble(a);
-
-	double inputB = Double.parseDouble(b);
-
-	String answer="";
-
-	switch (op) {
-
-	case "+": answer += inputA+inputB;
-
-	    break;
-
-	case "-": answer += inputA-inputB;
-
-	    break;
-
-	case "*": answer += inputA*inputB;
-
-	    break;
-
-	case "/": answer += inputA/inputB;
-
-	    break;
-
-	case "%": answer += inputA%inputB;
-
-	    break;
-
-	default: System.out.println( "INVALID. CURRENTLY THERE ARE ONLY:\n * + - / %");
-
-	}
-
-	return answer;
-
-    }
-
-
     private static boolean isOp(String op){
-
 	switch (op) {
-
 	case "+": return true;
-
 	case "-": return true;
-
 	case "*": return true;
-
 	case "/": return true;
-
 	case "%": return true;
-
 	default: return false;
-
 	}
-
     }
-
-
-
-
-    public static double eval (String s){
+    
+    private static String apply(String op, String b, String a){
+	double inputA = Double.parseDouble(a);
+	double inputB = Double.parseDouble(b);
+	String answer="";
+	switch (op){
+	case "+": answer += inputA+inputB;
+	    break;
+	case "-": answer += inputA-inputB;
+	    break;
+	case "*": answer += inputA*inputB;
+	    break;
+	case "/": answer += inputA/inputB;
+	    break;
+	case "%": answer += inputA%inputB;
+	    break;
+	default: System.out.println( "INVALID. CURRENTLY THERE ARE ONLY:\n * + - / %");
+	}
+	return answer;
+    }
+    
+    public static double eval(String s){
 	String [] tokens= s.split (" ");
-	Stack values = new Stack();
-	for (token :token){
-	    if (isOp (token)){
-		values.push (apply (token, values.pop (),values.pop));
+	Stack<String> values = new Stack<String>();
+	for (String a :tokens){
+	    if (isOp(a)){
+		values.push (apply (a, ""+values.pop(),""+values.pop()));
 	    }else {
-		values.push (token);
+		values.push(a);
 	    }
 	}
-	return Double.parseDouble (values.pop ());
+	return Double.parseDouble (""+values.pop ());
     }
 }
