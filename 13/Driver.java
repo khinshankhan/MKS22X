b279f6f09e12883a7ee86c8d7bb6f1748799fa5b
Khinshan Khan <kkhan@stuy.edu>
2017-05-04 19:50:35 -0400

    forgot to push

---
+++ 13/Driver.java
@@ -0,0 +1,7 @@
+public class Driver{
+    MyHeap test= new MyHeap(true);
+    for(int i=0; i<26; i++){
+	a.add('a'+i);
+    }
+    System.out.println(a);
+}
--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class MyHeap{
 
@@ -20,33 +21,55 @@ public class MyHeap{
     private int compare(String first, String other) {
 	return first.compareTo(other)*constant;
     }
-    
-    public int size() {
-	return size;
-    }
 
     public String peek() {
 	return heap.get(1);
     }
 
     public void add(String s){
-
+	heap.add(s);
+	pushUp(size);
+	return;
     }
 
     public String remove(){
+	if(size==0){
+	    throw new NoSuchElementException();
+	}
 	return "";
     }
 
-    private void pushUp(){
+    private void swap(int index1, int index2){
+	String temp= heap.get(index1);
+	heap.set(index1, heap.get(index2));
+	heap.set(index2, temp);
+    }
 
+    private void pushUp(int child){
+	int parent=child/2;
+	if(size==1){
+	    return;
+	}
+        if(compare(heap.get(child), heap.get(parent))>0){
+	    swap(child, parent);
+	    pushUp(parent);
+	}
+	return;
     }
     
     private void pushDown(){
 
     }
+    public String toString(){
+	String s="";
+	for(String x: heap){
+	    s+=x+",";
+	}
+	return s;
+    }
 
     private int height(){
-	return size()/2+1;
+	return size/2;
     }
     
 }
