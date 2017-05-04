926ec8c079b208395774ed20a95f87fb798c15ac
Khinshan Khan <kkhan@stuy.edu>
2017-05-03 22:00:32 -0400

    didnt push in class

---
+++ 13/MyHeap.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MyHeap{
+
+    boolean constant;
+    int size;
+    ArrayList<String>heap;
+
+    public MyHeap(){
+	heap=new ArrayList<String>();
+	heap.add("richu");
+	size=0;
+    }
+    
