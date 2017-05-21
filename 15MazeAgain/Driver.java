3454dad786cbd95cdf8ae379bd14de06f84d1c52
Khinshan Khan <kkhan@stuy.edu>
2017-05-21 10:21:55 -0400

    using shell script on dlee's generated mazes. pretty sure everything works

--- 15MazeAgain/Driver.java
+++ 15MazeAgain/Driver.java
@@ -1,16 +1,36 @@
 public class Driver{
+    private static final String CLEAR_SCREEN =  "\033[2J";
+    private static final String HIDE_CURSOR =  "\033[?25l";
+    private static final String SHOW_CURSOR =  "\033[?25h";
+    
+    private static void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+    
     public static void main(String args[]){
 	String a= args[0];
 	MazeSolver m = new MazeSolver(a, true);
+	System.out.println(CLEAR_SCREEN);
 	System.out.println("DFS\n");
+	wait(500);
         m.solve(0);
+	System.out.println(CLEAR_SCREEN);
 	System.out.println("BFS\n");
+	wait(500);
 	m = new MazeSolver(a, true);
 	m.solve(1);
+	System.out.println(CLEAR_SCREEN);
 	System.out.println("Best First\n");
+	wait(500);
 	m = new MazeSolver(a, true);
-	m.solve(2);	
+	m.solve(2);
+	System.out.println(CLEAR_SCREEN);
 	System.out.println("A*\n");
+	wait(500);
 	m = new MazeSolver(a, true);
 	m.solve(3);
     }
---
+++ 15MazeAgain/ShellTest15.sh
@@ -0,0 +1,3 @@
+for i in *.txt
+do java Driver $i
+done
